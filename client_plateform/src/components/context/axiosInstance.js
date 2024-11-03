84a8302365c376bdd6a4cf214f2445578757b76f
vaggu deekshith <vaggudeekshith@outlook.com>
2024-11-03 12:17:22 +0530

    client_plateform Done

---
+++ client_plateform/src/components/context/axiosInstance.js
@@ -0,0 +1,10 @@
+// src/utils/axiosInstance.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:4444', // Replace with your API server URL
+  //timeout: 10000, // Set a timeout (optional)
+});
+
+export default axiosInstance;
+
