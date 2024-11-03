44fa47a5ce6b604b65a6bc0c6c092e087adc78b6
vaggu deekshith <vaggudeekshith@outlook.com>
2024-11-03 20:19:43 +0530

    small update

--- client_plateform/src/components/sections/login_form.js
+++ client_plateform/src/components/sections/login_form.js
@@ -29,7 +29,7 @@ function LoginForm() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
       <div className="w-full max-w-sm p-8 space-y-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-700">Login</h2>
+        <h2 className="text-2xl font-bold text-center text-gray-700">Login to find Ur prospects</h2>
         {error && <p className="text-red-500">{error}</p>} {/* Display error message */}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
